a005e91348c065b50c9a097ff66866058b06691e
Mingdong Luo <mdluo@live.com>
2019-07-24 09:13:37 +0800

    test(base64): use jest.each

--- src/utils/base64.spec.ts
+++ src/utils/base64.spec.ts
@@ -1,16 +1,24 @@
 import * as base64 from './base64';
 
-const boolArray = '01010100011010000110010100100000011100010111010101101001011000110110101100100000'
-  .split('')
-  .map(bit => bit === '1');
-const base64String = 'VGhlIHF1aWNrIA';
+const table = [
+  [
+    '01010100011010000110010100100000011100010111010101101001011000110110101100100000',
+    'VGhlIHF1aWNrIA',
+  ],
+];
 
-test('encode', () => {
-  const encoded = base64.encode(boolArray);
-  expect(encoded).toEqual(base64String);
+function binaryToArray(input: string) {
+  return input.split('').map(bit => bit === '1');
+}
+
+test.each(table)('encode', (binaryStr, base64Str) => {
+  const booleanArr = binaryToArray(binaryStr);
+  const encoded = base64.encode(booleanArr);
+  expect(encoded).toEqual(base64Str);
 });
 
-test('decode', () => {
-  const decoded = base64.decode(base64String);
-  expect(decoded).toEqual(boolArray);
+test.each(table)('decode', (binaryStr, base64Str) => {
+  const booleanArr = binaryToArray(binaryStr);
+  const decoded = base64.decode(base64Str);
+  expect(decoded).toEqual(booleanArr);
 });
