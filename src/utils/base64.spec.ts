b8bbe39617391cc7edb1115549b9889b7c15bba1
Mingdong Luo <mdluo@live.com>
2019-07-24 20:16:35 +0800

    fix(base64): use native implementation

--- src/utils/base64.spec.ts
+++ src/utils/base64.spec.ts
@@ -1,24 +1,26 @@
 import * as base64 from './base64';
 
-const table = [
-  [
-    '01010100011010000110010100100000011100010111010101101001011000110110101100100000',
-    'VGhlIHF1aWNrIA',
-  ],
-];
+describe('binary <=> base64', () => {
+  const table = [
+    [
+      '01010100011010000110010100100000011100010111010101101001011000110110101100100000',
+      'VGhlIHF1aWNrIA',
+    ],
+  ];
 
-function binaryToArray(input: string) {
-  return input.split('').map(bit => bit === '1');
-}
+  function binaryToArray(input: string) {
+    return input.split('').map(bit => bit === '1');
+  }
 
-test.each(table)('encode', (binaryStr, base64Str) => {
-  const booleanArr = binaryToArray(binaryStr);
-  const encoded = base64.encode(booleanArr);
-  expect(encoded).toEqual(base64Str);
-});
+  test.each(table)('encode', (binaryStr, base64Str) => {
+    const booleanArr = binaryToArray(binaryStr);
+    const encoded = base64.encode(booleanArr);
+    expect(encoded).toEqual(base64Str);
+  });
 
-test.each(table)('decode', (binaryStr, base64Str) => {
-  const booleanArr = binaryToArray(binaryStr);
-  const decoded = base64.decode(base64Str);
-  expect(decoded).toEqual(booleanArr);
+  test.each(table)('decode', (binaryStr, base64Str) => {
+    const booleanArr = binaryToArray(binaryStr);
+    const decoded = base64.decode(base64Str);
+    expect(decoded).toEqual(booleanArr);
+  });
 });
--- src/utils/base64.ts
+++ src/utils/base64.ts
@@ -1,6 +1,31 @@
-import base64url from 'base64url';
+function base64UrlEncode(input: string) {
+  let encoded: string;
+  if (window && window.btoa) {
+    encoded = window.btoa(input);
+  } else {
+    encoded = Buffer.from(input).toString('base64');
+  }
+  encoded = encoded
+    .replace(/=/g, '')
+    .replace(/\+/g, '-')
+    .replace(/\//g, '_');
+  return encoded;
+}
+
+function base64UrlDecode(input: string) {
+  let decoded = input.replace(/-/g, '+').replace(/_/g, '/');
+  if (window && window.atob) {
+    decoded = window.atob(decoded);
+  } else {
+    decoded = Buffer.from(decoded, 'base64').toString();
+  }
+  return decoded;
+}
 
 export function encode(input: boolean[]) {
+  if (input.length % 8) {
+    throw new Error('Input length must be a integral multiple of 8');
+  }
   const byteGroup =
     input
       .map(b => (b ? '1' : '0'))
@@ -10,12 +35,12 @@ export function encode(input: boolean[]) {
     .map(b => parseInt(b, 2))
     .map(c => String.fromCharCode(c))
     .join('');
-  const encoded = base64url.encode(binaryString);
+  const encoded = base64UrlEncode(binaryString);
   return encoded;
 }
 
 export function decode(input: string) {
-  const decoded = base64url.decode(input);
+  const decoded = base64UrlDecode(input);
   return decoded
     .split('')
     .map(s =>
